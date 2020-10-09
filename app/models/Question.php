4bbf9b6608fcb6a03f45b2451f3a6b7d15fbd073
Alexandr Yakovlev <leffalt@gmail.com>
2020-10-09 22:14:28 +0700

    add question class

---
+++ app/models/Question.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Models;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class Question extends Model {
+    /**
+     * @var string
+     */
+    protected $table = 'questions';
+}
+
--- public/index.php
+++ public/index.php
@@ -1,3 +1,3 @@
 <?php
 
-require 'start.php';
+require '../start.php';
